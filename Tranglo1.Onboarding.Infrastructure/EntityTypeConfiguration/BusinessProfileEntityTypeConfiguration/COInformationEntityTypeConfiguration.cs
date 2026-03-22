f8c9fcf40d6713b0883ab3a76e126cad29884b5e
darren.koh <darrenkls90@gmail.com>
2026-03-22 16:41:01 +0800

    update reference

--- Tranglo1.Onboarding.Infrastructure/EntityTypeConfiguration/BusinessProfileEntityTypeConfiguration/COInformationEntityTypeConfiguration.cs
+++ Tranglo1.Onboarding.Infrastructure/EntityTypeConfiguration/BusinessProfileEntityTypeConfiguration/COInformationEntityTypeConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Tranglo1.Onboarding.Domain.Common;
 using Tranglo1.Onboarding.Domain.Entities;
 using Tranglo1.Onboarding.Infrastructure.Persistence;
 
